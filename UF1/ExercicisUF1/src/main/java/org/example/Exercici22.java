105672ce477cc7fc98cc4467c76b5eb7c90546ed
Santi Sabate <ssabate@iesebre.com>
2023-10-19 16:11:21 +0200

    Afegits programes a 18-10-2023

---
+++ UF1/ExercicisUF1/src/main/java/org/example/Exercici22.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercici22 {
+
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int n;
+
+        //Forcem a l'usuari a introduir un valor correcte
+        do{
+            System.out.println("Introduix un enter major que 0:");
+            n=ent.nextInt();
+            if(n>0) break;
+            else System.out.println("Ha de ser major major que 0!!");
+        }while(true);
+
+        //Primer dibuix
+        /*for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        */
+        //Segon dibuix
+        for (int i = 1; i <= n; i++) {
+            //Bucle que dibuixa espais en blanc
+            for (int j = n-i; j >=1 ; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            //Bucle que dibuixa espais en blanc
+            for (int j = n-i; j >=1 ; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
