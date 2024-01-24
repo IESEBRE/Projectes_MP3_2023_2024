3b3e33cc251c111bf8269654e75aba54d04371c0
Santi Sabate <ssabate@iesebre.com>
2024-01-24 12:28:39 +0100

    Actualitzat a 24/1/2024

--- UF2/UsaBiblioteca/src/main/java/org/example/Programa.java
+++ UF2/UsaBiblioteca/src/main/java/org/example/Programa.java
@@ -4,18 +4,29 @@ import java.util.Arrays;
 
 import static com.iesebre.mp3.uf2.library.Data.diaSeguent;
 import static com.iesebre.mp3.uf2.library.Data.diferenciaDies;
+import static com.iesebre.mp3.uf2.library.Numero.numeroCapicua;
+import static com.iesebre.mp3.uf2.library.Numero.revesNumero;
 
 public class Programa {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(diaSeguent(29,2,2024)));
-        System.out.println(diaSeguent(29,2,2023));
-        System.out.println(Arrays.toString(diaSeguent(31,5,2023)));
-        System.out.println(Arrays.toString(diaSeguent(31,12,2023)));
-        System.out.println(Arrays.toString(diaSeguent(19,1,2024)));
+//        System.out.println(Arrays.toString(diaSeguent(29,2,2024)));
+//        System.out.println(diaSeguent(29,2,2023));
+//        System.out.println(Arrays.toString(diaSeguent(31,5,2023)));
+//        System.out.println(Arrays.toString(diaSeguent(31,12,2023)));
+//        System.out.println(Arrays.toString(diaSeguent(19,1,2024)));
+//
+//        System.out.println(diferenciaDies(8,12,2005,23,1,2024));
 
-        System.out.println(diferenciaDies(8,12,2005,23,1,2024));
+//        System.out.println(revesNumero(123));
+//        System.out.println(revesNumero(120));
+//        System.out.println(revesNumero(-123));
+//        System.out.println(revesNumero(-120));
+
+        System.out.println(numeroCapicua(123));
+        System.out.println(numeroCapicua(1234321));
+        System.out.println(numeroCapicua(1234554321));
 
     }
 
