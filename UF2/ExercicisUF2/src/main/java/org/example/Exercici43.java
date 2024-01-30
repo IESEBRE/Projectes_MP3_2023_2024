a837f212399ed703bad33c2301499f9712865df8
Santi Sabate <ssabate@iesebre.com>
2024-01-30 13:02:20 +0100

    Actualitzat a 30/1/2024

--- UF2/ExercicisUF2/src/main/java/org/example/Exercici43.java
+++ UF2/ExercicisUF2/src/main/java/org/example/Exercici43.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Exercici43 {
 
+    public static void main(String[] args) {
+//        System.out.println(buscaParaula("ll la le li lo lu", "ll", 0));
+        System.out.println(buscaParaula("llo lly   lll  l l ", "l", 0));
+    }
+
     //Variables per compartir entre els mètodes
     static String texto, palabra;
     static int posicion;
@@ -19,26 +24,40 @@ public class Exercici43 {
             do {
                 tractarElement();
             } while (!elementFinal() && !trobat());
-            if (trobat()) tractamentTrobat();
-            else tractamentNoTrobat();
+            if (trobat()) return tractamentTrobat();
+            else return tractamentNoTrobat();
         }
     }
 
-    private static void tractamentNoTrobat() {
+    private static int tractamentNoTrobat() {
+        return -1;
     }
 
-    private static void tractamentTrobat() {
-
+    private static int tractamentTrobat() {
+        return ocurrencia;
     }
 
     private static boolean trobat() {
+        //Variables locals
+        String separadors=",.';:- ";
+
         //Mirem si el text trobat és una paraula aillada
         if(ocurrencia!=-1){ //Acabar este mètode
-
-
+            if(ocurrencia==0){      //hem trobat la paraula al principi del text
+                if(ocurrencia+palabra.length()==texto.length()      //hem trobat la paraula que a més acupa tot el text
+                        || separadors.contains(""+texto.charAt(ocurrencia+palabra.length()))){ //si el caracter de després de la
+                                                                                        // paraula és un separador vol dir que l'hem trobat
+                    return true;
+                }
+            }else{      //tractament de quan trobem la paraula no al principi del text
+                if(  separadors.contains(""+texto.charAt(ocurrencia-1)) && //mirem si la lletra anterior a la paraula és un separador
+                        (ocurrencia+palabra.length()==texto.length()      //hem trobat la paraula que a més acupa tot el text
+                        || separadors.contains(""+texto.charAt(ocurrencia+palabra.length())))){ //si el caracter de després de la
+                    // paraula és un separador vol dir que l'hem trobat
+                    return true;
+                }
+            }
         }
-
-
         return false;
     }
 
@@ -50,7 +69,8 @@ public class Exercici43 {
         //Anem a buscar la paraula dins del text a partir de la posició
         ocurrencia=texto.indexOf(palabra,posicion);
 
-
+        //Modifiquem el valor de posicio a buscar per evitar bucle infinit
+        posicion=ocurrencia+palabra.length();
     }
 
     private static int inicialitzarVariables(String text, String paraula, int posicio) {
