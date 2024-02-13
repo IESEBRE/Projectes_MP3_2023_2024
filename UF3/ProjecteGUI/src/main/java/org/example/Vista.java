20cfc44e570f6fc0f57c7a176d38626f0db9e13d
Santi Sabate <ssabate@iesebre.com>
2024-02-13 10:57:01 +0100

    Actualitzat projecte GUI al pati del  13/2/2024

--- UF3/ProjecteGUI/src/main/java/org/example/Vista.java
+++ UF3/ProjecteGUI/src/main/java/org/example/Vista.java
@@ -2,6 +2,10 @@ package org.example;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Vista extends JFrame{
     private JPanel panel;
@@ -23,7 +27,44 @@ public class Vista extends JFrame{
         //Anem a definir l'estructura de la taula
         DefaultTableModel model=new DefaultTableModel(new Object[]{"Nom","Pes"},0);
         taula.setModel(model);
+        //Forcem a que només se pugue seleccionar una fila de la taula
+        taula.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
+        insertarButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Comprovem que totes les caselles continguen informació
+                if(campNom.getText().isBlank() || campPes.getText().isBlank()){
+                    JOptionPane.showMessageDialog(null,"Falta omplir alguna dada!!");
+                }
+                else{
+                    model.addRow(new Object[]{campNom.getText(),campPes.getText()});
+                    campNom.setText("Pepito");
+                    campNom.setSelectionStart(0);
+                    campNom.setSelectionEnd(campNom.getText().length());
+                    campPes.setText("75");
+                    campNom.requestFocus();         //intentem que el foco vaigue al camp del nom
+                };
+            }
+        });
+        taula.addMouseListener(new MouseAdapter() {
+            /**
+             * {@inheritDoc}
+             *
+             * @param e
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                //Al seleccionar la taula omplim els camps de text en els valors de la fila seleccionada
+            }
+        });
     }
 
 
